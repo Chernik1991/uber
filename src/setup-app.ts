6e097738b751b53ce48c5c9a37d24b53badb1aaa
maksim.chernik <maksim.chernik@indev.by>
2025-08-26 14:24:49 +0300

    add vercel

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -6,9 +6,9 @@ import { videosRouter } from './videos/routers/videos.router';
 export const setupApp = (app: Express) => {
   app.use(express.json());
 
-  // app.get('/hometask_01/api/', (req: Request, res: Response) => {
-  //   res.status(200).send('hello world!!!');
-  // });
+  app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('hello world!!!');
+  });
 
   app.use('/hometask_01/api/videos', videosRouter);
   app.use('/hometask_01/api/testing', testingRouter);
